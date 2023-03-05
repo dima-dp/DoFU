7be19e442ad6d2a3b6cdf6b1f192e2012e7f21ad
Dima <ignatenkods2016@gmail.com>
2023-03-05 20:43:49 +0200

    Commit adds "loading screen" - Lottie-animated start of App and transition to CalcViewController

---
+++ DoFU/CalcViewConroller.swift
@@ -0,0 +1,29 @@
+//
+//  CalcViewConroller.swift
+//  DoFU
+//
+//  Created by Home on 05.03.2023.
+//
+
+import UIKit
+
+class CalcViewConroller: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- DoFU/ViewController.swift
+++ DoFU/ViewController.swift
@@ -9,12 +9,48 @@ import UIKit
 import Lottie
 
 class ViewController: UIViewController {
-
+    private var animationView: LottieAnimationView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupStartingAnimation()
     }
-
-
+    
+    
+    
+    
+    func setupStartingAnimation() {
+        animationView = .init(name: "liberate-ukraine")
+        animationView.frame = view.frame
+        animationView.contentMode = .scaleAspectFit
+        animationView.animationSpeed = 1
+        view.addSubview(animationView)
+        
+        animationView.play(toProgress: 0.983) {  [weak self] _ in
+            self?.moveToCalcVC()
+        }
+    }
+    
+    
+    private func moveToCalcVC() {
+        UIView.animate(withDuration: 1) { [weak self] in
+            let size = (self?.animationView.frame.size.width)! * 7
+            let diffX = size - (self?.animationView.frame.size.width)!
+            let diffY = (self?.animationView.frame.size.height)! - size
+            
+            self?.animationView.frame = CGRect(x: -(diffX/2), y: diffY/2, width: size, height: size)
+            
+            self?.animationView.alpha = 0
+            
+        } completion: { done in
+            if done {
+                let calcVC = CalcViewConroller()
+                calcVC.modalTransitionStyle = .crossDissolve
+                calcVC.modalPresentationStyle = .fullScreen
+                self.present(calcVC, animated: true)
+            }
+        }
+    }
 }
 
