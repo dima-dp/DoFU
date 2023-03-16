cde3e76a5475936f4b63b7ecbad4186379dcfa74
Dima <ignatenkods2016@gmail.com>
2023-03-16 23:07:59 +0200

    Commit adds work with buttons

--- DoFU/CalcViewConroller.swift
+++ DoFU/CalcViewConroller.swift
@@ -9,8 +9,26 @@ import UIKit
 import Realm
 
 class CalcViewConroller: UIViewController {
+    
+    let buttonsCornerRadius = 5.0
+    var stillTypying = false
+    var sumValue = ""
+    var date = Date()
+
 
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var sumLabel: UILabel! {
+        didSet {
+            sumLabel.layer.cornerRadius = buttonsCornerRadius
+        }
+    }
+    @IBOutlet var numberFromKeyboard: [UIButton]! {
+        didSet {
+            for button in numberFromKeyboard {
+                button.layer.cornerRadius = buttonsCornerRadius
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,8 +37,40 @@ class CalcViewConroller: UIViewController {
         tableView.dataSource = self
         tableView.backgroundColor = .darkGray
         tableView.layer.cornerRadius = 5
+
+    }
+    
+    
+    @IBAction func numberPressed(_ sender: UIButton) {
+        
+        let number = sender.currentTitle!
+        if stillTypying {
+            if sumLabel.text!.count < 10 {
+                sumLabel.text = sumLabel.text! + number
+            }
+        } else if number != "0" {
+            sumLabel.text = number
+            stillTypying = true
+        }
+    }
+    
+    @IBAction func cPressed(_ sender: UIButton) {
+        sumLabel.text = "0"
+        stillTypying = false
+    }
+    
+    @IBAction func savePressed(_ sender: UIButton) {
+        
+        sumValue = sumLabel.text!
+        date = Date()   // Add working with date
+        
+        sumLabel.text = "0"
+        stillTypying = false
     }
 }
+
+
+
 
 extension CalcViewConroller: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
